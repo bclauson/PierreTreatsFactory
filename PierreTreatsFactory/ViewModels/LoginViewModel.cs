3e707d89853cf77b65200fdf7344468855bca37a
bclauson <clauson.branden@gmail.com>
2022-04-09 17:39:52 -0700

    add account register, login, logout

---
+++ PierreTreatsFactory/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace PierreTreatsFactory.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
