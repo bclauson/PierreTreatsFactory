8f248e830fe20dd94cec88a2d197a15ab99ea864
bclauson <clauson.branden@gmail.com>
2022-04-09 18:24:42 -0700

    add detials edit and delete for Treat

---
+++ PierreTreatsFactory/Migrations/20220410012232_FixedModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierreTreatsFactory.Migrations
+{
+    public partial class FixedModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Flavors",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Flavors",
+                newName: "Description");
+        }
+    }
+}
